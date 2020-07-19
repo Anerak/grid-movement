9c36953d92cc0b1a8e7b4625dd621a78c124d2c5
Anerak <tobiastorresmdp@gmail.com>
2020-07-18 23:32:02 -0300

    New system to create the grid. Dynamic.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const divs = document.querySelectorAll('#holder div');
-const limits = {x: 9, y: 9} // Modify this when you resize the grid.
+let divs = [];
+let limits = {x: 9, y: 9} // Modify this when you resize the grid.
 
 class Player {
     constructor() {
@@ -15,22 +15,52 @@ class Player {
     }
 }
 
-// Check direction of the player based on key pressed..
+function initGrid(x, y) {
+  limits = {x: x - 1, y: y - 1};
+  if (divs.length > 0) {
+    divs.forEach((i) => i.removeEventListener('click', blockDiv));
+    divs = [];
+  }
+  let holder = document.getElementById('holder');
+  holder.innerHTML = null;
+  for (let i = 0; i < y; i++) {
+    let row = document.createElement('DIV');
+    row.classList.add('row');
+    for (let f = 0; f < x; f++) {
+      let div = document.createElement('DIV');
+      div.setAttribute('x',f);
+      div.setAttribute('y', i);
+      div.classList.add('square');
+      row.appendChild(div);
+    }
+    holder.appendChild(row);
+  }
+  divs = document.querySelectorAll('.row .square')
+  divs.forEach((i) => i.addEventListener('click', blockDiv));
+}
+
+// Check direction of the player based on key pressed.
 function movePjHandler (e) {
+  console.log(`X: ${pj.x} Y: ${pj.y}`);
     let direction = e.key;
     switch (direction) {
         case 'ArrowUp':
             if (checkBlock(pj.x, pj.y - 1)) {
-                pj.y = pj.y - 1 >= 0 ? pj.y - 1 : 0;
+              pj.y -= 1;
+              if (pj.y < 0) pj.y += 1;
+                //pj.y = pj.y - 1 >= 0 ? pj.y - 1 : 0;
             }
             break;
         case 'ArrowDown':
             if (checkBlock(pj.x, pj.y + 1)) {
-                pj.y = pj.y + 1 <= limits.y ? pj.y + 1 : limits.y;
+              pj.y += 1;
+              if (pj.y > limits.y) pj.y -= 1;
+                //pj.y = pj.y + 1 <= limits.y ? pj.y + 1 : limits.y;
             }
             break;
         case 'ArrowLeft':
             if (checkBlock(pj.x - 1, pj.y)) {
+                pj.x =
                 pj.x = pj.x - 1 >= 0 ? pj.x - 1 : 0;
             }
             break;
@@ -84,6 +114,6 @@ function blockDiv(e) {
 }
 
 window.addEventListener('keydown', movePjHandler);
-divs.forEach((i) => i.addEventListener('click', blockDiv));
 var pj = new Player;
+initGrid(3, 3);
 moveDivHandler();
